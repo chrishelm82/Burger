b98d6ccb894d3c428984a3ad7f6b0beccec65e76
The Matrix <helm.chris82@gmail.com>
2020-02-21 19:00:29 -0500

    added heroku

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,5 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
+-- CREATE DATABASE burgers_db;
+-- USE burgers_db;
 CREATE TABLE burgers(
   id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(50) NOT NULL,
